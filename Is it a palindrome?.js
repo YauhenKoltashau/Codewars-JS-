c07b42088152229c2473a33c56879dd7c4cc0531
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-03-09 12:49:32 +0300

    word is palidrom or not function, from an array to string using space between words

---
+++ Is it a palindrome?.js
@@ -0,0 +1,6 @@
+function isPalindrome(x) {
+    
+    return console.log(x.split('').reverse().join('').toLowerCase() === x.toLowerCase() ? true : false)
+ }
+
+  isPalindrome("aBbA")
---
+++ Sentence Smash.js
@@ -0,0 +1,7 @@
+function smash (words) {
+    let myString = words.join(' ')
+    return console.log(myString)
+ };
+ 
+
+ smash(['hello', 'world', 'this', 'is', 'great'])
