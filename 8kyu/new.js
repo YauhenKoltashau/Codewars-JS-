296015e881b0c9b9a6a449d5edf4f9163a79d1c1
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-04-01 12:47:10 +0300

    corrected file new.js

---
+++ 8kyu/new.js
@@ -0,0 +1,9 @@
+function testFactorial(a) {
+    let x;
+    // Тут нужно написать решение
+    for (let i = 1; i <= a; i++) {
+        x = 1 * i
+    }
+    return x;
+}
+console.log(testFactorial(7))
